be12102bc6c6ed14798911f21cbe971b0e5efa91
Parisa <73034495+parisamhmd@users.noreply.github.com>
2021-08-12 09:59:13 +0430

    install material ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { Button } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core"
+const useStyles = makeStyles({
+    button: {
+        backgroundColor: "pink "
+    }
+})
 function App() {
+    const classes = useStyles();
     return (
-        <div >
-
+        <div style={{ height: "100vh" }}>
+            <Button className={classes.button}>sdfsdfd</Button>
         </div>
     );
 }
