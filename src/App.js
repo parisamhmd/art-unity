d3d60bb4c14656d96e347c5ecd32519d658a999f
Parisa <73034495+parisamhmd@users.noreply.github.com>
2021-08-12 21:41:45 +0430

    install react query

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import { Button } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core"
+import "./App.css";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import { QueryClient, QueryClientProvider } from "react-query";
 const useStyles = makeStyles({
-    button: {
-        backgroundColor: "pink "
-    }
-})
+  button: {
+    backgroundColor: "pink ",
+  },
+});
 function App() {
-    const classes = useStyles();
-    return (
-        <div style={{ height: "100vh" }}>
-            <Button className={classes.button}>sdfsdfd</Button>
-        </div>
-    );
+  const classes = useStyles();
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div style={{ height: "100vh" }}>
+        <Button className={classes.button}>sdfsdfd</Button>
+      </div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
