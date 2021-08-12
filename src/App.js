44aafd66ccd972afb25d2612be951bad87353c55
Parisa <73034495+parisamhmd@users.noreply.github.com>
2021-08-12 13:52:53 +0430

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from 'react';
+
+function App() {
+  //   const queryClient = new QueryClient();
+  return (
+    <div style={{ height: "100vh" }}>
+      {/* <QueryClientProvider client={queryClient}> */}
+      {/* <Button>sdfsdfd</Button> */}
+      {/* </QueryClientProvider> */}
+    </div>
+  );
+}
+
+export default App;
